7691d9f1c578aeab773270b3373b493dbb24535f
WhatsWrongOB <whatswrongobaid@gmail.com>
2025-08-15 15:36:27 +0000

    -project completed

--- src/models/attendance.model.js
+++ src/models/attendance.model.js
@@ -17,7 +17,7 @@ const attendanceSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ["Present", "Absent"],
+    enum: ["Present", "Absent", "Leave"],
     required: true,
   },
 });
